084a31d154af4d5c012d145ab52cdcb1095caa60
那一年的风 <842463784@qq.com>
2022-10-11 22:30:55 +0800

    add data source configration

--- src/main/java/com/edu/murray/controller/com/edu/murray/com/edu/murray/controller/HelloController.java
+++ src/main/java/com/edu/murray/controller/com/edu/murray/com/edu/murray/controller/HelloController.java
@@ -1,18 +1,31 @@
 package com.edu.murray.controller.com.edu.murray.com.edu.murray.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.edu.murray.controller.com.edu.murray.MySqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author hp
  */
 
 @RestController
+@ResponseBody
 public class HelloController {
+    @Autowired
+    private Environment env;
 
-    @RequestMapping("/hello")
+    @Autowired
+    private MySqlDataSource dataSource;
+
+    @GetMapping(value = "hello")
     public String hello(){
-        return "Hello spring boot!";
+        System.out.println("Hello Murray!");
+        System.out.println(env.getProperty("user1.age"));
+        System.out.println(env.getProperty("user1.name"));
+        System.out.println(dataSource.toString());
+        return "{\"name\": \"Murray\", \"tempDir\": tempDir}";
     }
 }
