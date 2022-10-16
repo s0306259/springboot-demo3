28d0fff619f7c271400e0fb6fded4e4c5fa09776
那一年的风 <842463784@qq.com>
2022-10-16 22:09:33 +0800

    add test cases

--- src/test/java/com/edu/murray/SpringBootDemo01ApplicationTests.java
+++ src/test/java/com/edu/murray/SpringBootDemo01ApplicationTests.java
@@ -1,8 +1,6 @@
 package com.edu.murray;
 
 import com.edu.murray.dao.CustomerDao;
-import com.edu.murray.dao.EmployeeDao;
-import com.edu.murray.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +10,7 @@ public class SpringBootDemo01ApplicationTests {
     @Autowired
     CustomerDao customerDao;
 
+    @Test
     public void testEmployeeDao(){
         System.out.println(customerDao.selectById("00013530066F40F9B341516B12C638A7"));
     }
